8970af7a63e2dbd7c4112970ac3b5750885c9ff2
Matt Welsh <matt@fixie.ai>
2023-06-09 20:35:10 -0700

    Working on this.

--- octofarm/game.py
+++ octofarm/game.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import math
 import random
 from rich.console import Console
 
@@ -11,25 +12,47 @@ WORLD_HEIGHT = 40
 
 class Food:
     next_id = 0
-    def __init__(self, glyph: str, x: int, y: int, value: int, health: int):
+
+    def __init__(
+        self, world: "World", glyph: str, x: int, y: int, value: int, health: int
+    ):
         self.id = Food.next_id
-        self.glyph = glyph
         Food.next_id += 1
+
+        self.world = world
+        self.glyph = glyph
         self.x = x
         self.y = y
         self.value = value
         self.health = health
+        self.max_health = health
 
     def move_to(self, x: int, y: int):
-        self.x = min(WORLD_WIDTH-1, max(x, 0))
-        self.y = min(WORLD_HEIGHT-1, max(y, 0))
+        self.x = min(WORLD_WIDTH - 1, max(x, 0))
+        self.y = min(WORLD_HEIGHT - 1, max(y, 0))
+
+    def under_attack(self):
+        return self in self.world.octopus.tentacles
 
     def render(self):
-        return self.glyph
+        if self.under_attack():
+            return f"[red]{self.glyph}[/red]"
+        else:
+            return self.glyph
 
 
 class FoodGroup:
-    def __init__(self, glyph: str, center_x: int, center_y: int, radius: int, num_foods: int, value: int):
+    def __init__(
+        self,
+        world: "World",
+        glyph: str,
+        center_x: int,
+        center_y: int,
+        radius: int,
+        num_foods: int,
+        value: int,
+    ):
+        self.world = world
         self.glpyh = glyph
         self.center_x = center_x
         self.center_y = center_y
@@ -37,6 +60,7 @@ class FoodGroup:
         self.value = value
         self.foods = [
             Food(
+                world,
                 glyph,
                 center_x + random.randint(-radius, radius),
                 center_y + random.randint(-radius, radius),
@@ -52,15 +76,70 @@ class FoodGroup:
 
 
 class Octopus:
-    def __init__(self, world, x: int, y: int, tentacles: int, reach: int):
+    def __init__(
+        self, world, x: int, y: int, tentacles: int, reach: int, attack_power: int
+    ):
         self.world = world
         self.x = x
         self.y = y
-        self.tentacles = tentacles
+        self.num_tentacles = tentacles
         self.reach = reach
+        self.attack_power = attack_power
+
+        self.tentacles = [None for _ in range(tentacles)]
+
+    def distance(self, x, y) -> float:
+        return math.sqrt(abs(self.x - x) ** 2 + abs(self.y - y) ** 2)
+
+    def can_reach(self, x, y) -> bool:
+        return self.distance(x, y) <= self.reach
+
+    def move(self):
+        """Move the Octopus. This will be overridden by subclasses."""
+        pass
 
     def update(self):
-        pass
+        # Execute move.
+        self.move()
+
+        # Check if our tentacles are still in range.
+        for index, tentacle in enumerate(self.tentacles):
+            if tentacle is not None and not self.can_reach(tentacle.x, tentacle.y):
+                # Food went out of range.
+                tentacle.health = tentacle.max_health
+                self.tentacles[index] = None
+
+        # Sort foods by distance to ourselves.
+        foods = sorted(
+            [
+                food
+                for foodgroup in self.world.foodgroups
+                for food in foodgroup.foods
+                if self.can_reach(food.x, food.y)
+            ],
+            key=lambda food: self.distance(food.x, food.y),
+        )
+        for food in foods:
+            if food in self.tentacles:
+                # We're already grabbing it.
+                continue
+            grabbed = False
+            for index, tentacle in enumerate(self.tentacles):
+                if tentacle is None:
+                    # Started to grab it.
+                    grabbed = True
+                    self.tentacles[index] = food
+                    break
+            if not grabbed:
+                # No free tentacles.
+                break
+
+        # Attack.
+        for tentacle in self.tentacles:
+            if tentacle:
+                tentacle.health -= self.attack_power
+                # XXX MDW - Check for death, update score.
+
 
     def render(self):
         return "🐙"
@@ -68,28 +147,33 @@ class Octopus:
 
 class World:
     def __init__(self):
-        self._octopus = Octopus(self, WORLD_WIDTH // 2, WORLD_HEIGHT // 2, 8, 3)
-        self._foodgroups = []
-        self._foodgroups.append(FoodGroup("🐟", 5, 5, 5, 5, 10))
-        self._foodgroups.append(FoodGroup("🐠", 30, 15, 3, 5, 10))
-        self._foodgroups.append(FoodGroup("🐡", 20, 30, 4, 10, 10))
+        self.octopus = Octopus(self, WORLD_WIDTH // 2, WORLD_HEIGHT // 2, 8, 12, 1)
+        self.foodgroups = [
+            FoodGroup(self, "🐟", 5, 5, 5, 5, 10),
+            FoodGroup(self, "🐠", 30, 15, 3, 5, 10),
+            FoodGroup(self, "🐡", 20, 30, 4, 10, 10),
+        ]
         self.moves = 0
         self.cells = [[None for _ in range(WORLD_WIDTH)] for _ in range(WORLD_HEIGHT)]
 
     def move_octopus(self, row: int, col: int):
-        self._octopus.x = min(WORLD_WIDTH-1, max(self._octopus.x + col, 0))
-        self._octopus.y = min(WORLD_HEIGHT-1, max(self._octopus.y + row, 0))
+        self.octopus.x = min(WORLD_WIDTH - 1, max(self.octopus.x + col, 0))
+        self.octopus.y = min(WORLD_HEIGHT - 1, max(self.octopus.y + row, 0))
 
     def update(self):
         self.moves += 1
         self.last_cells = self.cells
         self.cells = [[None for _ in range(WORLD_WIDTH)] for _ in range(WORLD_HEIGHT)]
 
-        for foodgroup in self._foodgroups:
+        # Update food.
+        for foodgroup in self.foodgroups:
             foodgroup.update()
             for food in foodgroup.foods:
                 self.cells[food.y][food.x] = food
-        self.cells[self._octopus.y][self._octopus.x] = self._octopus
+        self.cells[self.octopus.y][self.octopus.x] = self.octopus
+
+        # Update octopus.
+        self.octopus.update()
 
         # Return only the cells that have changed.
         changed_cells = []
@@ -101,4 +185,3 @@ class World:
 
     def at(self, row, col):
         return self.cells[row][col]
-        
--- octofarm/main.py
+++ octofarm/main.py
@@ -12,10 +12,25 @@ from game import World
 from ui import OctofarmUI
 
 
-@click.command()
+@click.group()
 def main():
+    pass
+
+
+@main.command()
+def run():
     world = World()
     OctofarmUI(world).run()
+
+
+@main.command()
+@click.option("--num-iterations", default=10000, help="Number of iterations to run")
+def simulate(num_iterations: int):
+    console.print(f"Simulating {num_iterations} iterations...")
+    world = World()
+    for _ in range(num_iterations):
+        world.update()
+    console.print(f"Done.")
 
 
 if __name__ == "__main__":
