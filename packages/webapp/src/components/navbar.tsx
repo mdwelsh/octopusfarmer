59b9bb8c14349910594cc88d000efcc49d083fed
Matt Welsh <matt@fixie.ai>
2023-11-11 19:17:48 -0800

    Add new icon.

--- packages/webapp/src/components/navbar.tsx
+++ packages/webapp/src/components/navbar.tsx
@@ -1,13 +1,15 @@
 'use client';
 
 import Link from 'next/link';
+import Image from 'next/image';
+
 
 export default function NavBar() {
 	return (
 		<div className="flex flex-row justify-between items-center border-slate-500 border-b-2 pl-6 pt-4 pb-4">
 			<div className="flex flex-row items-center">
 				<Link href="/">
-					<p className="font-mono">🐙🌾 Octopus Farmer</p>
+					<Image src="/icon.png" alt="Octopus Farmer" width={100} height={100} />
 				</Link>
 			</div>
 			<div className="font-mono flex flex-row pr-8 underline">
