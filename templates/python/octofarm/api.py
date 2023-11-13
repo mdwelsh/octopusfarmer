cd606b50ea75bb9a20804c06647aa05a2d1ad5b5
Matt Welsh <matt@fixie.ai>
2023-11-12 16:50:06 -0800

    Working on this.

---
+++ templates/python/octofarm/api.py
@@ -0,0 +1,95 @@
+"""This module defines the types used by the Octopus Farmer game API."""
+
+import dataclasses
+import enum
+from typing import Dict, List, Optional
+
+import dataclasses_json
+
+
+class CamelCaseJsonMixin(dataclasses_json.DataClassJsonMixin):
+    """Utility class for conversion to/from camel case for field names."""
+    dataclass_json_config = dataclasses_json.config(
+        letter_case=dataclasses_json.LetterCase.CAMEL
+    )[
+        "dataclasses_json"
+    ]  # type: ignore
+
+
+@dataclasses_json.dataclass_json
+@dataclasses.dataclass
+class GameType(enum.Enum):
+    """Represents the game difficulty."""
+
+    TEST = "test"
+    NORMAL = "normal"
+    HARD = "hard"
+    INSANE = "insane"
+
+
+@dataclasses_json.dataclass_json
+@dataclasses.dataclass
+class NewGameRequest(CamelCaseJsonMixin):
+    """A request to create a new game."""
+
+    owner: str
+    game_type: Optional[GameType] = None
+    seed: Optional[int] = None
+
+
+@dataclasses_json.dataclass_json
+@dataclasses.dataclass
+class TentacleData(CamelCaseJsonMixin):
+    """The fish ID being grabbed by this tentacle."""
+
+    fish_id: Optional[str] = None
+
+
+@dataclasses_json.dataclass_json
+@dataclasses.dataclass
+class FishData(CamelCaseJsonMixin):
+    """Represents a single fish."""
+
+    id: str
+    x: int
+    y: int
+    value: int
+    health: int
+
+
+@dataclasses_json.dataclass_json
+@dataclasses.dataclass
+class OctopusData(CamelCaseJsonMixin):
+    """Represents the state of the octpus."""
+
+    x: int
+    y: int
+    speed: int
+    reach: int
+    attack: int
+    tentacles: List[Optional[TentacleData]]
+
+
+@dataclasses_json.dataclass_json
+@dataclasses.dataclass
+class WorldData(CamelCaseJsonMixin):
+    """The overall world state."""
+
+    width: int
+    height: int
+    moves: int
+    score: int
+    octopus: OctopusData
+    fish: List[FishData]
+
+
+@dataclasses_json.dataclass_json
+@dataclasses.dataclass
+class GameData(CamelCaseJsonMixin):
+    """State for a single game."""
+
+    game_id: str
+    owner: str
+    created: str
+    modified: str
+    world: WorldData
---
+++ templates/python/octofarm/client.py
@@ -0,0 +1,110 @@
+import dataclasses
+from typing import Callable, Optional
+
+import requests
+from rich.console import Console
+from rich.progress import track
+from rich.progress import Progress, SpinnerColumn, TimeElapsedColumn
+from octofarm import api
+
+console = Console()
+
+
+@dataclasses.dataclass
+class OctopusPosition:
+    """Represents the desired position of the Octopus returned by an OctopusFunction."""
+
+    x: int
+    y: int
+
+
+OctopusFunction = Callable[[api.GameData], OctopusPosition]
+"""An OctopusFunction takes the current GameData as an argument, and returns
+the desired position of the Octopus."""
+
+
+class Client:
+    """A simple client for the Octopus Farmer game."""
+
+    def __init__(
+        self,
+        url: str,
+        game_id: Optional[str] = None,
+        new_game: Optional[api.NewGameRequest] = None,
+    ):
+        self._url = url
+        if game_id:
+            self._game_id = game_id
+            self._game = self._fetch_game()
+        else:
+            assert new_game
+            self._game = self._new_game(new_game)
+            self._game_id = self._game.game_id
+
+    @property
+    def score(self) -> int:
+        return self._game.world.score
+
+    @property
+    def moves(self) -> int:
+        return self._game.world.moves
+
+    @property
+    def world(self) -> api.WorldData:
+        return self._game.world
+
+    def preview_url(self) -> str:
+        """Returns the URL of the game visualizer for this game."""
+        return f"{self._url}/game/{self._game_id}"
+
+    def _new_game(self, req: api.NewGameRequest) -> api.GameData:
+        """Start a new game."""
+        import json
+        print(f"REQ IS: {json.dumps(req.to_dict())}")
+        resp = requests.post(f"{self._url}/api/games", json=req.to_dict())
+        resp.raise_for_status()
+        return api.GameData.from_dict(resp.json())
+
+    def _fetch_game(self) -> api.GameData:
+        """Fetch an existing game."""
+        resp = requests.get(f"{self._url}/api/game/{self._game_id}")
+        resp.raise_for_status()
+        return api.GameData.from_dict(resp.json())
+
+    def moveTo(self, x: int, y: int) -> api.GameData:
+        resp = requests.post(
+            f"{self._url}/api/game/{self._game_id}",
+            json={"moves": self._game.world.moves, "octopus": {"x": x, "y": y}},
+        )
+        resp.raise_for_status()
+        game = api.GameData.from_dict(resp.json())
+        self._game = game
+        return game
+
+    def step(self, octopus: OctopusFunction) -> api.GameData:
+        """Run a single step of the game."""
+        new_position = octopus(self._game)
+        return self.moveTo(new_position.x, new_position.y)
+
+    def run(self, octopus: OctopusFunction, steps: int) -> api.GameData:
+        """Run the game for the given number of steps."""
+
+        console.print(
+            f"Starting game {self._game_id} - live display: [green]{self.preview_url()}[/green]"
+        )
+        progress = Progress(
+            SpinnerColumn(),
+            *Progress.get_default_columns(),
+            TimeElapsedColumn(),
+        )
+        with progress:
+            task = progress.add_task(
+                f"[green]Running game for {steps} steps...", total=steps
+            )
+            for _ in range(steps):
+                self.step(octopus)
+                progress.update(task, advance=1)
+
+        console.print(f"Final score: {self.score}")
+
+        return self._game
